611237d74a3a06462eb84f05ed9fd9e820b0d15c
Werayoot <the_oilman43436@hotmail.com>
2021-05-09 20:21:15 +0700

    Update Exercise4_Werayoot_Ph.py

--- Exercise4_Werayoot_Ph.py
+++ Exercise4_Werayoot_Ph.py
@@ -1,1 +1,8 @@
-
+F_Eng = 75.0
+G_Business = 73.5
+I_Com = 75.5
+C_Program = 71.2
+print("---Your Score---")
+print("Foundation English :", F_Eng)
+print("General Business :", G_Business)
+print("Introduction to Computer System :", I_Com)
